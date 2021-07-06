801ecb8540434a720a1cd42b1aa9f82fc5d9f591
brosk014 <brosk014@umn.edu>
2021-07-05 21:28:09 -0500

    Added examples to LinkedIndices comments

--- Runtime/Scripts/General/LinkedIndices.cs
+++ Runtime/Scripts/General/LinkedIndices.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 namespace IVLab.Plotting
 {
     /// <summary>
-    /// This class provides an "index space" where each index has additional attributes/data attached to it.
+    /// This class provides an "index space" wherein each index is allowed to have additional 
+    /// attributes/data attached to it, such as whether or not that index (and the data that correlates
+    /// to it) is highlighted or masked.
     /// </summary>
     public class LinkedIndices
     {
-        /// <summary> Total number of indices (and "data points"). </summary>
         private int _size;
-        /// <summary> Automatically toggled flag that indicates if any attributes have been changed. </summary>
         private bool _linkedAttributesChanged;
         /// <summary> Array of attributes linked to the indices. </summary>
         private LinkedAttributes[] _linkedAttributes;
@@ -29,12 +29,12 @@ namespace IVLab.Plotting
             }
         }
 
-        /// <summary> Gets <see cref="_size"/>. </summary>
+        /// <summary> Total number of indices (and "data points"). </summary>
         public int Size
         {
             get => _size;
         }
-        /// <summary> Gets and sets <see cref="_linkedAttributesChanged"/>. </summary>
+        /// <summary> Automatically toggled flag that indicates if any attributes have been changed. </summary>
         public bool LinkedAttributesChanged
         {
             get => _linkedAttributesChanged;
@@ -50,17 +50,15 @@ namespace IVLab.Plotting
         }
 
         /// <summary>
-        /// This class acts as a container for attributes attached to an individual index.
+        /// This class acts as a container for the attributes attached to each individual index, as used
+        /// by <see cref="LinkedIndices"/>.
         /// </summary>
         public class LinkedAttributes
         {
             /// <summary> Reference to the linked indices array that the linked attribute is a part of. </summary>
             private LinkedIndices _linkedIndices;
-            /// <summary> Flags whether or not this index is highlighted (selected). </summary>
-            private bool _highlighted;  // Flags whether or not this index is highlighted (selected)
-            /// <summary> Flags whether or not this index is masked (filtered). </summary>
+            private bool _highlighted;
             private bool _masked;
-            /// <summary> Automatically flags whether or not this attribute has changed. </summary>
             private bool _linkedAttributeChanged;
 
             /// <summary>
@@ -75,10 +73,10 @@ namespace IVLab.Plotting
                 _linkedAttributeChanged = false;
             }
 
-            /// <summary> Gets and sets <see cref="_highlighted"/>, 
-            /// automatically toggling <see cref="_linkedAttributeChanged"/> and
-            /// <see cref="_linkedAttributesChanged"/> to true if the value
-            /// is changed. </summary>
+            /// <summary> Flags whether or not this index is highlighted (selected),
+            /// automatically toggling <see cref="LinkedAttributeChanged"/> and
+            /// <see cref="LinkedAttributesChanged"/> to true if the value
+            /// is indeed changed. </summary>
             public bool Highlighted
             {
                 get { return _highlighted; }
@@ -94,10 +92,11 @@ namespace IVLab.Plotting
                 }
             }
 
-            /// <summary> Gets and sets <see cref="_masked"/>, 
-            /// automatically toggling <see cref="_linkedAttributeChanged"/> and
-            /// <see cref="_linkedAttributesChanged"/> to true if the value
-            /// is changed. </summary>
+
+            /// <summary> Flags whether or not this index is masked (filtered),
+            /// automatically toggling <see cref="LinkedAttributeChanged"/> and
+            /// <see cref="LinkedAttributesChanged"/> to true if the value
+            /// is indeed changed. </summary>
             public bool Masked
             {
                 get { return _masked; }
@@ -113,7 +112,39 @@ namespace IVLab.Plotting
                 }
             }
 
-            /// <summary> Gets and sets <see cref="_linkedAttributeChanged"/>. </summary>
+            /// <summary> 
+            /// A flag that signals whether or not any of the linked attributes have
+            /// changed. This flag is automatically set to true when an attribute changes, but
+            /// it must manually be set to false. 
+            /// </summary>
+            /// <example>
+            /// For example, a common sequence of use for this attribute would occur along the lines of:
+            /// <code>
+            /// "At some point, the 'Highlighted' attribute of linked index i is set to true."
+            /// "If this attribute was previously false, i.e we are now changing its value, its LinkedAttributeChanged flag will automatically be set to true"
+            /// "In other words, there's no need for us to directly set `linkedIndices[i].LinkedAttributeChanged = true;`"
+            /// 
+            /// linkedIndices[i].Highlighted = true;
+            /// 
+            ///             . 
+            ///             . 
+            ///             .
+            /// 
+            /// "Later on, we can choose to work with the linked index attributes of i only if they have changed"
+            /// if (linkedIndices[i].LinkedAttributeChanged) {
+            ///     if (linkedIndices[i].Highlighted) {
+            ///         Debug.Log("Index " + i + " changed to highlighted state.");
+            ///     }
+            ///     else
+            ///     {
+            ///         Debug.Log("Index " + i + " changed to un-highlighted state.");
+            ///     }
+            ///     
+            ///     "We do have to be sure to toggle the flag back to false after we use it, however"
+            ///     linkedIndices[i].LinkedAttributeChanged = false;
+            /// }
+            /// </code>
+            /// </example>
             public bool LinkedAttributeChanged
             {
                 get => _linkedAttributeChanged;
