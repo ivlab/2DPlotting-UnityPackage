a0e92d500e412ec625f0a1b86c25c07f95fa3170
brosk014 <brosk014@umn.edu>
2021-07-22 16:33:26 -0500

    Added method to DataManager that can save selected data points to new csv

--- Runtime/Scripts/Data Management/DataManager.cs
+++ Runtime/Scripts/Data Management/DataManager.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -272,14 +273,65 @@ namespace IVLab.Plotting
             print(selectedIDs);
         }
 
-        public void SaveSelectedDataToCSV()
+        /// <summary>
+        /// Saves all the currently selected data points to a new csv.
+        /// </summary>
+        /// <param name="filePath">Path and name of csv file to save data in.</param>
+        /// <param name="saveRowNames">Whether or not to save a first column with row names.</param>
+        /// <remarks>
+        /// The csv this method creates can be used to create a data table with only that data!
+        /// </remarks>
+        public void SaveSelectedDataToCSV(string filePath, bool saveRowNames = true)
         {
+            using (StreamWriter streamWriter = new StreamWriter(filePath))
+            {
+                // Create the header
+                string[] header;
+                if (saveRowNames)
+                {
+                    header = new string[dataTable.ColumnNames.Length + 1];
+                    header[0] = "Data Point ID";
+                    dataTable.ColumnNames.CopyTo(header, 1);
+                }
+                else
+                {
+                    header = dataTable.ColumnNames;
+                }
+                // Write the header
+                streamWriter.WriteLine(string.Join(",", header));
 
-        }
+                // Write the rest of data in the table
+                for (int i = 0; i < linkedIndices.Size; i++)
+                {
+                    string[] row;
+                    if (saveRowNames)
+                    {
+                        row = new string[dataTable.Width + 1];
+                        row[0] = dataTable.RowNames[i];
+                    }
+                    else
+                    {
+                        row = new string[dataTable.Width];
+                    }
 
-        public void LoadDataFromCSV()
-        {
+                    if (linkedIndices[i].Highlighted)
+                    {
+                        for (int j = 0; j < dataTable.Width; j++)
+                        {
+                            if (saveRowNames)
+                            {
+                                row[j + 1] = dataTable.Data(i, j).ToString();
+                            }
+                            else
+                            {
+                                row[j] = dataTable.Data(i, j).ToString();
+                            }
+                        }
 
+                        streamWriter.WriteLine(string.Join(",", row));
+                    }
+                }
+            }
         }
     }
 }
